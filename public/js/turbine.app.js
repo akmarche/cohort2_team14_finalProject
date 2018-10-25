981fa65375aa63f2ff57684e2ef6e7e73d3567b4
akmarche <42591895+akmarche@users.noreply.github.com>
2018-10-25 19:38:09 -0400

    Created fetchSite() method in turbine.app.js to collect turbine site information.

--- public/js/turbine.app.js
+++ public/js/turbine.app.js
@@ -27,5 +27,14 @@ var turbineApp = new Vue({
   },
   created () {
     this.fetchTurbine();
+  },
+  fetchSite () {
+    fetch('api/site.php')
+    .then( response => response.json() )
+    .then( json => {turbineApp.site = json} )
+    .catch( err => {
+      console.log('TASK FETCH ERROR');
+      console.log(err);
+    })
   }
 })
