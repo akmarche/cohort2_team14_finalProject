478442baa715a5b0db0e964974d18fab8e11cf9a
Mayur Dhaniwala <mayur.dec16@gmail.com>
2018-10-25 20:42:14 -0400

    Removing redundant code

--- public/api/client.php
+++ public/api/client.php
@@ -1,10 +1,6 @@
 <?php
 require '../../app/common.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  require 'clientPost.php';
-  exit;
-}
 // 1. Go to the database and get all work associated with the $taskId
 $clients = Client::fetchAll();
 // 2. Convert to JSON
