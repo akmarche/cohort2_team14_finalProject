0fd64a221cf815e2788e8c9c05640424956c9af7
Mayur Dhaniwala <mayur.dec16@gmail.com>
2018-10-29 16:24:34 -0400

    Updating KPI API's

--- app/data/work-stub.sql
+++ app/data/work-stub.sql
@@ -93,3 +93,13 @@ CREATE TABLE SensorTimeSeries(
     PRIMARY KEY (sensorDeployedId, dataCollectedDate),
     CONSTRAINT sensorDeployedId_fk FOREIGN KEY (sensorDeployedId) REFERENCES SensorDeployed (sensorDeployedId)
 );
+
+
+CREATE VIEW heatRate AS
+SELECT sd.sensorId, s.heatRate, td.turbineId
+FROM SensorTimeSeries s, SensorDeployed sd, TurbineDeployed td
+WHERE s.sensorDeployedId = sd.sensorDeployedId AND td.turbineDeployedId = sd.TurbineDeployedId;
+
+Drop View If Exists heatRate;
+
+Select * from heatRate;
