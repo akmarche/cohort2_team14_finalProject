0a71c51c21f71d398f87ff6abddff37bbb354f72
akmarche <42591895+akmarche@users.noreply.github.com>
2018-10-29 16:00:35 -0400

    Test preparation. Created TurbineOutput Chart methods and edited Highcharts formatting.

--- public/js/turbine.app.js
+++ public/js/turbine.app.js
@@ -23,10 +23,6 @@ var turbineApp = new Vue({
         console.log('TASK FETCH ERROR:');
         console.log(err);
       } )
-      .catch( err => {
-        console.log('TURBINE CAPACITY FETCH ERROR');
-        console.log(err);
-      })
     },
   },
 
@@ -34,10 +30,14 @@ var turbineApp = new Vue({
     fetch('api/sensorTimeSeriesPost.php')
     .then( response => response.json() )
     .then( json => {
-      turbineApp.capacity = json;
+      turbineApp.output = json;
       this.formatTurbineOutput();
       this.buildTurbineOutputChart();
     })
+    .catch( err => {
+      console.log('TURBINE OUTPUT FETCH ERROR');
+      console.log(err);
+    } )
   },
 
   formatTurbineOutput() {
@@ -50,17 +50,17 @@ var turbineApp = new Vue({
             type: 'line'
         },
         title: {
-            text: 'Monthly Average Temperature'
+            text: 'Daily Turbine Output'
         },
         subtitle: {
-            text: 'Source: WorldClimate.com'
+            text: 'Source: sensorTimeSeries.csv'
         },
         xAxis: {
             categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         },
         yAxis: {
             title: {
-                text: 'Temperature (°C)'
+                text: 'Units of Energy'
             }
         },
         plotOptions: {
@@ -72,10 +72,13 @@ var turbineApp = new Vue({
             }
         },
         series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            name: 'Turbine 1',
+            data: this.output.map( turbines => [turbines.output] )
         }, {
-            name: 'London',
+            name: 'Turbine 3',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }, {
+            name: 'Turbine 4',
             data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
         }]
     });
