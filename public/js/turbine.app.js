a49563a6cf2bbec2589f08338b324c0428493366
akmarche <42591895+akmarche@users.noreply.github.com>
2018-10-29 15:33:00 -0400

    Preliminary Highcharts Structure.

--- public/js/turbine.app.js
+++ public/js/turbine.app.js
@@ -22,9 +22,65 @@ var turbineApp = new Vue({
       .catch( err => {
         console.log('TASK FETCH ERROR:');
         console.log(err);
+      } )
+      .catch( err => {
+        console.log('TURBINE CAPACITY FETCH ERROR');
+        console.log(err);
       })
     },
   },
+
+  fetchTurbineOutputKPI() {
+    fetch('api/sensorTimeSeriesPost.php')
+    .then( response => response.json() )
+    .then( json => {
+      turbineApp.capacity = json;
+      this.formatTurbineOutput();
+      this.buildTurbineOutputChart();
+    })
+  },
+
+  formatTurbineOutput() {
+
+  },
+
+  buildTurbineOutputChart() {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Monthly Average Temperature'
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com'
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }]
+    });
+  },
+
   created () {
     this.fetchTurbine();
   },
