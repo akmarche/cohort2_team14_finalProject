1dcc4995220d7e64a32aa259331b966e38c48064
Mayur Dhaniwala <mayur.dec16@gmail.com>
2018-10-29 16:44:59 -0400

    Bug fix

--- public/js/turbine.app.js
+++ public/js/turbine.app.js
@@ -27,61 +27,59 @@ var turbineApp = new Vue({
         console.log(err);
       } )
     },
-  },
+    fetchTurbineOutputKPI() {
+      fetch('api/heatrate.php')
+      .then( response => response.json() )
+      .then( json => {
+        turbineApp.output = json;
+        this.formatTurbineOutput();
+        this.buildTurbineOutputChart();
+      })
+      .catch( err => {
+        console.log('TURBINE OUTPUT FETCH ERROR');
+        console.log(err);
+      } )
+    },
 
-  fetchTurbineOutputKPI() {
-    fetch('api/heatrate.php')
-    .then( response => response.json() )
-    .then( json => {
-      turbineApp.output = json;
-      this.formatTurbineOutput();
-      this.buildTurbineOutputChart();
-    })
-    .catch( err => {
-      console.log('TURBINE OUTPUT FETCH ERROR');
-      console.log(err);
-    } )
-  },
-
-  formatTurbineOutput() {
-    this.output.forEach(
-      (entry, index, arr) => {
-        entry.date = Date.parse(entry.date);
+    formatTurbineOutput() {
+      this.output.forEach(
+        (entry, index, arr) => {
+          entry.date = Date.parse(entry.date);
+        });
+    },
+    buildTurbineOutputChart() {
+      var turbineOutputChart = Highcharts.chart('chart', {
+          chart: {
+              type: 'area'
+          },
+          title: {
+              text: 'Daily Turbine Output'
+          },
+          subtitle: {
+              text: ''
+          },
+          xAxis: {
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+          },
+          yAxis: {
+              title: {
+                  text: 'Units of Energy'
+              }
+          },
+          plotOptions: {
+              line: {
+                  dataLabels: {
+                      enabled: true
+                  },
+                  enableMouseTracking: false
+              }
+          },
+          series: [{
+              name: 'Turbine 1',
+              data: this.output.map( turbines => [turbines.output] )
+          }]
       });
-  },
-
-  buildTurbineOutputChart() {
-    var turbineOutputChart = Highcharts.chart('chart', {
-        chart: {
-            type: 'area'
-        },
-        title: {
-            text: 'Daily Turbine Output'
-        },
-        subtitle: {
-            text: ''
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        yAxis: {
-            title: {
-                text: 'Units of Energy'
-            }
-        },
-        plotOptions: {
-            line: {
-                dataLabels: {
-                    enabled: true
-                },
-                enableMouseTracking: false
-            }
-        },
-        series: [{
-            name: 'Turbine 1',
-            data: this.output.map( turbines => [turbines.output] )
-        }]
-    });
+    },
   },
 
   created () {
