d45bdc5c00d5b398751c5b41b6721392028b93a3
akmarche <42591895+akmarche@users.noreply.github.com>
2018-11-02 16:46:52 -0400

    Cleaning up code and adding turbine info html

--- public/js/turbine.app.js
+++ public/js/turbine.app.js
@@ -11,12 +11,6 @@ var turbineApp = new Vue({
       maintenanceInterval : ''
     }
   ],
-  output: {
-    turbineId: '',
-    heatRate: '',
-    sensorId: '',
-    dataCollectedDate: ''
-  }
   },
   computed: {
   },
@@ -30,79 +24,9 @@ var turbineApp = new Vue({
         console.log(err);
       } )
     },
-    fetchTurbineOutputKPI() {
-      fetch('api/heatrate.php')
-      .then( response => response.json() )
-      .then( json => {
-        turbineApp.output = json;
-        //this.formatTurbineOutput();
-        this.buildTurbineOutputChart();
-      })
-      .catch( err => {
-        console.log('TURBINE OUTPUT FETCH ERROR');
-        console.log(err);
-      } )
-    },
-
-    formatTurbineOutput() {
-      this.output.forEach(
-        (entry, index, arr) => {
-          entry.dataCollectedDate = Date.parse(entry.dataCollectedDate);
-        });
-    },
-    buildTurbineOutputChart() {
-      var turbineOutputChart = Highcharts.chart('chart', {
-          title: {
-              text: 'Daily Turbine Output'
-          },
-          subtitle: {
-              text: ''
-          },
-          xAxis: {
-              type: 'datetime'
-          },
-          yAxis: {
-              title: {
-                  text: 'Units of Energy'
-              }
-          },
-          plotOptions: {
-                area: {
-                    fillColor: {
-                        linearGradient: {
-                            x1: 0,
-                            y1: 0,
-                            x2: 0,
-                            y2: 1
-                        },
-                        stops: [
-                            [0, Highcharts.getOptions().colors[0]],
-                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                        ]
-                    },
-                    marker: {
-                        radius: 2
-                    },
-                    lineWidth: 1,
-                    states: {
-                        hover: {
-                            lineWidth: 1
-                        }
-                    },
-                    threshold: null
-                }
-            },
-          series: [{
-              type: 'area',
-              name: 'Turbine 1',
-              data: this.output.map( item => [item.heatRate] )
-          }]
-      });
-    },
   },
 
   created () {
     this.fetchTurbine();
-    this.fetchTurbineOutputKPI();
   },
 })
